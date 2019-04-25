394488b444f68f320dee658ff4d4e65083ad73a7
Hayden Bass <haydenbass@Haydens-MacBook-Pro.local>
2019-04-25 10:33:21 -0700

    passing more tests

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,7 +1,6 @@
 var changeVideo = (video) => ({
-  //TODO:  Return some action object to change the currently playing video.
   'type': 'CHANGE_VIDEO',
-  'video': video,
+  'video': video
 });
 
 export default changeVideo;
